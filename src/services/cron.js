46e9c4a6c53c712e7a273ca466f47918fff30113
borabekmezci <borabekmezci.com>
2022-01-19 22:51:25 +0300

    subscription fixed

--- src/services/cron.js
+++ src/services/cron.js
@@ -11,37 +11,36 @@ dotenv.config({
   });
 
 const price = process.env.PRICE;
-const interval = parseInt(process.env.INTERVAL);// 1 day of Interval for every succesfull subscriptions
 
 const scheduleSubscriptionPayment = () => {
   // Every day at 00:00:00
-  cron.schedule("0 * * * * *", async() => {//checking every minute for test purpose!
+  cron.schedule("0 0 0 * * *", async() => {//checking every minute for test purpose!
     console.log("Running a task every day, date:", new Date());
-    const sub = await subscriptionDB.find();
 
-    if(sub){
-        for (let i = 0;sub[i].nextDate<=new Date();i++){//if sub[0].nextdate > sub[1].nextdate ==>not working!
-            const user = await userDB.findOne({userID : sub[i].userID});
-            const iyzicoResult = await makePayment(price , user);
-            
-            if (iyzicoResult.status === 'success'){//succesfull payments recording to subcriptionDB collection
+    const subscriptions = await subscriptionDB.find({nextDate : {$lt : new Date()}});
+
+        subscriptions.forEach(async (sub)  => {
+            const user = await userDB.findOne({userID : sub.userID})
+            const iyzicoResult = await makePayment( price , user);
+            if (iyzicoResult.status === 'success'){//succesfull payments are recorded to subcriptionDB collection
                 const subInfo = {
-                    nextDate : new Date(sub[i].nextDate.getTime() + interval)
+                    nextDate : new Date(sub.nextDate.getTime() + (parseInt(sub.interval)*parseInt(86400000)))
                 }
-                const subUpdated = await subscriptionDB.findOneAndUpdate({userID : sub[i].userID},subInfo, {new : true});
-                console.log(subUpdated);
+                const subUpdated = await subscriptionDB.findOneAndUpdate({userID : sub.userID},subInfo, {new : true});
+                console.log('Existing subscription is updated to : ',subUpdated);
             }
-            const paymentDBInfo = {// every payment of subscriptions whether or not succesfull are recording to paymentDB
+            const paymentDBInfo = {// every payment of subscriptions whether or not succesfull are recorded to paymentDB
                 paymentID: iyzicoResult.paymentId,
-                userID: sub[i].userID,
+                userID: sub.userID,
                 status: iyzicoResult.status
             }
-            const payment = await paymentDB(paymentDBInfo);
-            db.collection('paymentdbs').insertOne(payment);
-            console.log(payment);
-        }
-    }
-  });
+            paymentDB.create(paymentDBInfo)
+            .then(payment => {
+                console.log('Payment Result is Saved : ',payment);
+            })
+            .catch(err => {console.log('There has been error while updating paymnet result!')});
+        }); 
+    });  
 };
 
 module.exports = scheduleSubscriptionPayment;
